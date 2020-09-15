2a500bc4e034da307c8404f8dc63f16cd446bf41
JimSunJing <jimsun6428@gmail.com>
2020-09-15 23:59:30 +0800

    exercise 3.1-3.6

---
+++ index.js
@@ -0,0 +1,97 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+
+let phonebook = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendick',
+    number: '39-23-6423122'
+  },
+]
+
+app.use(express.json())
+
+// exercise 3.1
+app.get('/api/persons', (req, res) => {
+  res.json(phonebook)
+})
+
+// exercise 3.2
+app.get('/info', (req,res)=>{
+  res.send(`
+    <div>Phonebook has info for ${phonebook.length} people</div>
+    <div>${new Date()}</div>
+  `)
+})
+
+// exercise 3.3
+app.get('/api/persons/:id', (req,res)=>{
+  const id = Number(req.params.id)
+  console.log(id);
+  const person = phonebook.find(p=>p.id === id)
+  console.log(person);
+
+  person ? res.json(person)
+    : res.status(404).end()
+})
+
+// exercise 3.4 
+app.delete('/api/persons/:id', (req,res)=>{
+  const id = Number(req.params.id)
+  phonebook = phonebook.filter(p => p.id !== id)
+  res.status(204).end()
+})
+
+const getRandomInt = max => {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+// exercise 3.5
+app.post('/api/persons', (req,res) => {
+  const body = req.body
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: "need to provide name."
+    })
+  }else if (!body.number){
+    return res.status(400).json({
+      error: "need to provide number."
+    })
+  }else if (phonebook.find(p => p.name === body.name)){
+    return res.status(400).json({
+      error: "name must be unique"
+    })
+  }
+
+  const newP = {
+    id: getRandomInt(10000),
+    name: body.name,
+    number: body.number
+  }
+
+  phonebook = phonebook.concat(newP)
+  res.json(newP)
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+  console.log(`server run on port ${PORT}`);
+})
