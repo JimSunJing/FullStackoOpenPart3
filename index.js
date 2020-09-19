b4456bfb417946fc9676d8746fa8f887f67ce2f9
JimSunJing <jimsun6428@gmail.com>
2020-09-19 17:27:53 +0800

    exercise 3.14

--- index.js
+++ index.js
@@ -5,20 +5,19 @@ const Person = require('./modules/phonebook')
 
 const app = express()
 
-app.use(express.json())
-
 // 显示静态资源的中间件
 app.use(express.static('build'))
+app.use(express.json())
 
 // exercise 3.8
 morgan.token('postData', req => {
-  return Object.keys(req.body).length === 0 && req.body.constructor === Object 
-    ? ' ' 
+  return Object.keys(req.body).length === 0 && req.body.constructor === Object
+    ? ' '
     : JSON.stringify(req.body)
 })
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 
+// ---------- 后端逻辑部分 ---------------
 app.get('/api/persons', (req, res) => {
   // exercise 3.13
   Person.find({}).then(data => {
@@ -27,7 +26,7 @@ app.get('/api/persons', (req, res) => {
 })
 
 // exercise 3.2
-app.get('/info', (req,res)=>{
+app.get('/info', (req, res) => {
   Person.find({}).then(data => {
     res.send(`
       <div>Phonebook has info for ${data.length} people</div>
@@ -36,52 +35,78 @@ app.get('/info', (req,res)=>{
   })
 })
 
-app.get('/api/persons/:id', (req,res)=>{
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(data => {
-      res.json(data)
+      if (data) {
+        res.json(data)
+      } else {
+        res.status(404).end()
+      }
     })
+    .catch(e => next(e))
 
 })
 
 // exercise 3.4 
-app.delete('/api/persons/:id', (req,res)=>{
-  const id = Number(req.params.id)
-  phonebook = phonebook.filter(p => p.id !== id)
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {res.status(204).end()})
+    .catch(err => next(err))
 })
 
 const getRandomInt = max => {
   return Math.floor(Math.random() * Math.floor(max))
 }
 
-// exercise 3.5
-app.post('/api/persons', (req,res) => {
+// exercise 3.14
+app.post('/api/persons', (req, res) => {
   const body = req.body
 
   if (!body.name) {
     return res.status(400).json({
       error: "need to provide name."
     })
-  }else if (!body.number){
+  } else if (!body.number) {
     return res.status(400).json({
       error: "need to provide number."
     })
-  }else if (phonebook.find(p => p.name === body.name)){
-    return res.status(400).json({
-      error: "name must be unique"
-    })
   }
+  // else if (phonebook.find(p => p.name === body.name)){
+  //   return res.status(400).json({
+  //     error: "name must be unique"
+  //   })
+  // }
 
-  const newP = {
-    id: getRandomInt(10000),
+  const newP = new Person({
     name: body.name,
     number: body.number
-  }
+  })
 
-  phonebook = phonebook.concat(newP)
-  res.json(newP)
+  newP.save().then(data => {
+    res.json(data)
+  })
 })
+
+// ------------ 后端事件处理结束 -------------
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+// 替代express的原生错误处理器
+const errorHandler = (err, req, res, next) => {
+  console.error(err.message)
+
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    return res.status(400).send({error: 'malformatted id'})
+  }
+  
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
